36189d2b706ec154a4079956cd7bf63e982f2d87
JulianLiu <liu.julian@gmail.com>
2017-11-17 15:04:46 +0800

    Add license MIT

--- lptags.user.js
+++ lptags.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name         Launchpad bug tags helper
 // @namespace    https://launchpad.net/~julian-liu
-// @version      2.4
+// @version      2.5
+// @license      MIT
 // @description  LP bugs tags helper
 // @author       Julian Liu
 // @match        https://bugs.launchpad.net/*/+filebug
